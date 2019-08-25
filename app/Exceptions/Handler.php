789fa9c9ca758a71d01dada487695a36d40d45cc
Alper <alpercevik@outlook.com>
2019-08-25 13:56:12 +0200

    add Json response for generic exceptions to Lumen exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,14 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Response;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -28,8 +31,9 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -39,12 +43,47 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param Exception $e
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
+        $rendered = parent::render($request, $e);
+
+        if($rendered instanceof JsonResponse) {
+            return $rendered;
+        }
+
+        $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+        $message = "Server Error";
+        $error = "";
+
+        if ($e instanceof HttpResponseException) {
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = 'HTTP_INTERNAL_SERVER_ERROR';
+        } elseif ($e instanceof MethodNotAllowedHttpException) {
+            $status = Response::HTTP_METHOD_NOT_ALLOWED;
+            $message = 'HTTP_METHOD_NOT_ALLOWED';
+        } elseif ($e instanceof NotFoundHttpException) {
+            $status = Response::HTTP_NOT_FOUND;
+            $message = 'HTTP_NOT_FOUND';
+        } elseif ($e instanceof AuthorizationException) {
+            $status = Response::HTTP_FORBIDDEN;
+            $message = 'HTTP_FORBIDDEN';
+        } elseif ($e instanceof ValidationException && $e->getResponse()) {
+            $status = Response::HTTP_BAD_REQUEST;
+            $error = $e->getResponse()->getData();
+            $message = 'HTTP_BAD_REQUEST';
+        } elseif ($e instanceof InvalidUnitException) {
+            $status = Response::HTTP_UNPROCESSABLE_ENTITY;
+            $message = 'Invalid unit type';
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'error' => $error,
+        ], $status);
     }
 }
